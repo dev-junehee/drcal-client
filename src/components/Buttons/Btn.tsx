a6a3912db59ea809f23875fb264bdc1f6af017b7
junehee <dev.junehee@gmail.com>
2023-07-27 20:58:02 +0900

    :sparkles: Feat: 페이지용 버튼 구현

---
+++ src/components/Buttons/Btn.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Btn = ({ content, handler }) => {
+  return <Container onClick={handler}>{content}</Container>;
+};
+
+export default Btn;
+
+const Container = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 21rem;
+  height: 3.5em;
+  border: 0;
+  outline: 0;
+  border-radius: 8px;
+  background-color: ${props => props.theme.primary};
+  color: ${props => props.theme.white};
+`;
