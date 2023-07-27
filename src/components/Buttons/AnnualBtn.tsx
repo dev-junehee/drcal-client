e32db9dbfb0ed6579908cde6552a1fb4726d2848
junehee <dev.junehee@gmail.com>
2023-07-27 21:19:26 +0900

    :sparkles: Feat: 휴가 신청 버튼 구현

---
+++ src/components/Buttons/AnnualBtn.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+const AnnualBtn = () => {
+  return <Container>휴가 신청</Container>;
+};
+
+export default AnnualBtn;
+
+const Container = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 15rem;
+  height: 3.5em;
+  border: 0;
+  outline: 0;
+  border-radius: 8px;
+  background-color: ${props => props.theme.secondary};
+  color: ${props => props.theme.white};
+  cursor: pointer;
+  transition: all ease 0.3s;
+  &:hover {
+    opacity: 0.9;
+  }
+`;
