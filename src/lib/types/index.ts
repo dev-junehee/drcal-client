949b2b77f3bad2697f2669649a58cb46cebeb046
junehee <dev.junehee@gmail.com>
2023-07-28 16:35:31 +0900

    :books: Docs: type 파일 생성

---
+++ src/lib/types/index.ts
@@ -0,0 +1,23 @@
+// 프로젝트에 사용되는 types 정리 //
+
+// 사용자 데이터
+interface UserData {
+  id: number;
+  emp_no: number;
+  name: string;
+  email: string;
+  phone: string;
+  hospital_id: number;
+  dept_id: number;
+  level: string;
+  auth: string;
+  status: string;
+  annual: number;
+  duty: number;
+  profile_image_url: string;
+  hiredate: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export { UserData };
