c66daeed059a50c49d85e5975ffe6dfdceac324c
idey <evxrafter@gmail.com>
2023-07-28 15:35:00 +0900

    :sparkles: Feat: 로그인 페이지 구현 시작

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <h1>어서오세요!</h1>
+      <span>이메일</span>
+      <input type="text" placeholder="이메일을 입력해주세요." />
+      <span>비밀번호</span>
+      <input type="password" placeholder="비밀번호를 입력해주세요." />
+      <input type="submit" value="로그인" />
+      <div>
+        <span>아직 계정이 없으신가요?</span>
+        <Link to="/signup">회원가입</Link>
+      </div>
+    </>
+  );
 };
 
 export default Login;
