be3ff4d6442534fdd7abb397a522c9eea332199c
junehee <dev.junehee@gmail.com>
2023-07-29 23:30:49 +0900

    :sparkles: Feat: 유효성 검사 코드 추가

--- src/lib/Validation/index.ts
+++ src/lib/Validation/index.ts
@@ -1,7 +1,29 @@
-export const emailValidation = {
+const emailValidation = {
   required: '이메일은 필수 입력입니다.',
   pattern: {
     value: /\S+@\S+\.\S+/,
     message: '이메일 형식에 맞지 않습니다.',
   },
 };
+
+const nameValidation = {
+  required: '이름은 필수 입력입니다.',
+  minLength: {
+    value: 3,
+    message: '이름은 3자 이상입니다.',
+  },
+};
+
+const PWValidation = {
+  required: '비밀번호는 필수 입력입니다.',
+  pattern: {
+    value: /^[A-za-z0-9]*$/,
+    message: '영문과 숫자만 가능합니다.',
+  },
+  minLength: {
+    value: 8,
+    message: '비밀번호는 8 ~ 16자입니다.',
+  },
+};
+
+export { emailValidation, nameValidation, PWValidation };
