954cca5e4c6e775d91d972d1e531e6b457aa9cda
junehee <dev.junehee@gmail.com>
2023-08-01 21:34:52 +0900

    :sparkles: Feat: 회원가입 페이지 유효성 검사 추가

--- src/lib/Validation/index.ts
+++ src/lib/Validation/index.ts
@@ -6,14 +6,6 @@ const emailValidation = {
   },
 };
 
-const nameValidation = {
-  required: '이름은 필수 입력입니다.',
-  minLength: {
-    value: 3,
-    message: '이름은 3자 이상입니다.',
-  },
-};
-
 const PWValidation = {
   required: '비밀번호는 필수 입력입니다.',
   pattern: {
@@ -22,8 +14,40 @@ const PWValidation = {
   },
   minLength: {
     value: 8,
-    message: '비밀번호는 8 ~ 16자입니다.',
+    message: '비밀번호는 8 ~ 20자입니다.',
   },
 };
 
-export { emailValidation, nameValidation, PWValidation };
+const nameValidation = {
+  required: '이름은 필수 입력입니다.',
+  minLength: {
+    value: 2,
+    message: '이름은 2자 이상입니다.',
+  },
+};
+
+const hospitalValidation = {
+  required: '재직 병원은 필수 입력입니다.',
+  // pattern: {
+  //   value: /^[가-힣]*$/,
+  //   message: '영문과 숫자만 가능합니다.',
+  // },
+};
+
+const deptValidation = {
+  required: '근무 파트는 필수 입력입니다.',
+  // pattern: {
+  //   value: /^[가-힣]*$/,
+  //   message: '영문과 숫자만 가능합니다.',
+  // },
+};
+
+const phoneValidation = {
+  required: '연락처는 필수 입력입니다.',
+  minLength: {
+    value: 10,
+    message: '연락처는 10자 이상입니다.',
+  },
+};
+
+export { emailValidation, PWValidation, nameValidation, hospitalValidation, deptValidation, phoneValidation };
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,13 +1,26 @@
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router';
-import { emailValidation } from '@/lib/Validation';
+import {
+  emailValidation,
+  PWValidation,
+  nameValidation,
+  phoneValidation,
+  hospitalValidation,
+  deptValidation,
+} from '@/lib/Validation';
 import Btn from '@/components/Buttons/Btn';
 import styled from 'styled-components';
+import { hospitalDecode } from '@/utils/decode';
+import axios from 'axios';
 
 interface SignUpBody {
   email: string;
   password: string;
-  pwConfirm: string;
+  pwCheck: string;
+  name: string;
+  hospital: string;
+  dept: string;
+  phone: string;
 }
 
 const SignUp = () => {
@@ -15,55 +28,127 @@ const SignUp = () => {
     register,
     handleSubmit,
     watch,
-    formState: { errors, isSubmitting, isDirty, isValid },
-    reset,
+    formState: { errors, isSubmitting },
   } = useForm<SignUpBody>({ mode: 'onChange' });
 
   const navigate = useNavigate();
 
+  console.log(
+    '병원명 출력 확인',
+    Object.values(hospitalDecode).map(v => v.hospital),
+  );
+
+  console.log(
+    '해당 병원의 파트 출력 확인',
+    Object.values(hospitalDecode).map(v => v.dept),
+  );
+
+  // 회원가입 핸들러
+  const userSignUp = ({ email, password, name, hospital, dept, phone }: SignUpBody) => {
+    const body = {
+      email,
+      password,
+      name,
+      hospital,
+      dept,
+      phone,
+    };
+    axios.post('/user/register', body).then(res => {
+      if (res.status === 200) {
+        console.log('회원가입 성공', res);
+        if (confirm('회원가입 성공!\n로그인 페이지로 이동하시겠습니까?')) {
+          navigate('/login');
+        }
+      }
+    });
+    console.log('회원가입 정보 확인', body);
+  };
+
   return (
     <Container>
-      <SignUpFormContainer>
+      <SignUpFormContainer onSubmit={handleSubmit(userSignUp)}>
         <Title>
           <h2>회원가입</h2>
         </Title>
         <InfoContainer>
           <InfoWrapper>
             <span>가입 정보</span>
+
             <Label>
               Email
-              <Input type="email" placeholder="kim@hospital.dr" {...register('email', emailValidation)} />
+              {errors?.email && <Error>{errors.email.message}</Error>}
+              <Input type="email" placeholder="kim@doctor.kr" {...register('email', emailValidation)} />
             </Label>
             <Label>
               Password
-              <Input type="password" placeholder="8자 이상의 비밀번호를 입력해주세요." />
+              {errors?.password && <Error>{errors.password.message}</Error>}
+              <Input
+                type="password"
+                maxLength={20}
+                placeholder="8자 이상의 비밀번호를 입력해 주세요."
+                {...register('password', PWValidation)}
+              />
             </Label>
             <Label>
               Password Check
-              <Input type="password" placeholder="비밀번호를 다시 입력해주세요." />
+              {errors?.pwCheck && <Error>{errors.pwCheck.message}</Error>}
+              <Input
+                type="password"
+                placeholder="비밀번호를 다시 입력해 주세요."
+                {...register('pwCheck', {
+                  required: '비밀번호 확인은 필수 입력입니다.',
+                  validate: {
+                    value: (pw: string | undefined) => {
+                      if (watch('password') !== pw) return '비밀번호가 일치하지 않습니다.';
+                    },
+                  },
+                })}
+              />
             </Label>
           </InfoWrapper>
           <InfoWrapper>
             <span>유저 정보</span>
             <Label>
-              Name
-              <Input type="text" placeholder="김의사" />
+              name
+              {errors?.name && <Error>{errors.name.message}</Error>}
+              <Input type="text" placeholder="김의사" maxLength={10} {...register('name', nameValidation)} />
             </Label>
             <Label>
               Hospital
-              <Input type="text" placeholder="연세 세브란스 병원" />
+              {errors?.hospital && <Error>{errors.hospital.message}</Error>}
+              <Select {...register('hospital', hospitalValidation)}>
+                <option value="" selected disabled hidden>
+                  재직중인 병원을 선택해 주세요.
+                </option>
+                {Object.values(hospitalDecode).map(v => (
+                  <option value={v.hospital}>{v.hospital}</option>
+                ))}
+              </Select>
             </Label>
             <Label>
               Part
-              <Input type="text" placeholder="응급의학과" />
+              {errors?.dept && <Error>{errors.dept.message}</Error>}
+              <Select {...register('dept', deptValidation)}>
+                <option className="default" value="" selected disabled hidden>
+                  근무중인 파트를 선택해 주세요.
+                </option>
+                <option value="응급의학과">응급의학과</option>
+                <option value="산부인과">산부인과</option>
+              </Select>
             </Label>
             <Label>
               Phone Number
-              <Input type="text" placeholder="010-1234-5678" />
+              {errors?.phone && <Error>{errors.phone.message}</Error>}
+              <Input
+                type="text"
+                placeholder="하이픈(-) 없이 입력하세요."
+                maxLength={11}
+                {...register('phone', phoneValidation)}
+              />
             </Label>
           </InfoWrapper>
         </InfoContainer>
-        <Btn content="회원가입" />
+        <Btn content="회원가입" disabled={isSubmitting} />
 
         <AlreadyAccount>
           <span>계정이 이미 있으신가요?</span>
@@ -89,7 +174,7 @@ const Container = styled.div`
   align-items: center;
   width: 100%;
   height: 100%;
-  padding-right: 1rem;
+  padding-right: 16px;
 `;
 
 const SignUpFormContainer = styled.form`
@@ -97,10 +182,11 @@ const SignUpFormContainer = styled.form`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 1rem;
+  gap: 16px;
   width: 900px;
   height: 900px;
   border-radius: 10px;
+  margin-right: 20px;
   background-color: ${props => props.theme.white};
 `;
 
@@ -111,46 +197,73 @@ const Title = styled.div`
   }
 `;
 
+const InfoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  gap: 64px;
+  margin: 30px 0 30px;
+`;
+
 const InfoWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  gap: 1rem;
+  width: 320px;
+  gap: 16px;
   span {
     font-weight: 600;
-    margin-bottom: 1.2rem;
+    margin-bottom: 20px;
   }
 `;
 
-const InfoContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  gap: 4rem;
-  margin: 4rem 0 4rem;
-`;
-
 const Label = styled.label`
-  width: 16rem;
+  width: 256px;
   font-family: 'ABeeZee', sans-serif;
   font-size: 0.8rem;
 `;
 
 const Input = styled.input`
-  width: 16rem;
-  height: 2.5rem;
-  padding-left: 1rem;
+  padding-left: 16px;
   border: 1px solid ${props => props.theme.gray};
-  border-radius: 0.5rem;
-  margin-top: 0.5rem;
+  border-radius: 8px;
+  margin-top: 8px;
   font-family: 'Pretendard', sans-serif;
+`;
+
+const Select = styled.select`
+  padding-left: 16px;
+  border: 1px solid ${props => props.theme.gray};
+  border-radius: 8px;
+  margin-top: 8px;
+  font-family: 'Pretendard', sans-serif;
+  /* 브라우저 기본 디자인 숨기기 */
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  /* 새로운 화살표 디자인 추가 */
+  background: url(/arrow.png) calc(100% - 16px) center no-repeat;
+  background-size: 14px;
+  background-color: ${props => props.theme.white};
+  color: gray;
+`;
+
+const Error = styled.span`
+  margin-left: 10px;
+  font-size: 0.7rem;
+  color: ${props => props.theme.red};
 `;
 
 const AlreadyAccount = styled.div`
   display: flex;
-  gap: 0.5rem;
+  gap: 8px;
   font-size: 0.9rem;
   .login {
     font-weight: 700;
     color: ${props => props.theme.primary};
+    cursor: pointer;
+    &:hover {
+      opacity: 0.9;
+    }
   }
 `;
