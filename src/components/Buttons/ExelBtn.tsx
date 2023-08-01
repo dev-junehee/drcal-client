5dddbeee568baf805b52a0c261ac3fd96743ac2d
junehee <dev.junehee@gmail.com>
2023-08-01 23:18:44 +0900

    :lipstick: Design: 버튼 컴포넌트 width, height 단위 수정

--- src/components/Buttons/ExelBtn.tsx
+++ src/components/Buttons/ExelBtn.tsx
@@ -10,8 +10,8 @@ const Container = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 8rem;
-  height: 2rem;
+  width: 140px;
+  height: 30px;
   border: 0;
   border-radius: 8px;
   background-color: ${props => props.theme.primary};
--- src/components/Buttons/ModalBtn.tsx
+++ src/components/Buttons/ModalBtn.tsx
@@ -10,8 +10,8 @@ const Container = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 24rem;
-  height: 3.5em;
+  width: 390px;
+  height: 60px;
   border: 0;
   border-radius: 8px;
   background-color: ${props => props.theme.primary};
