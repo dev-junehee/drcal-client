a96b3c26f9ac25c9c931935053f1756615385f51
junehee <dev.junehee@gmail.com>
2023-08-02 15:35:55 +0900

    :lipstick: Select 전역 스타일 추가

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -33,7 +33,7 @@ const GlobalStyle = createGlobalStyle`
       opacity: 0.9;
     }
   }
-  input, select {
+  input {
     box-sizing: border-box;
     width: 320px;
     height: 46px;
@@ -46,6 +46,37 @@ const GlobalStyle = createGlobalStyle`
       border: 1px solid ${props => props.theme.secondary};
       box-shadow: 0 0 6px 3px rgba(156, 184, 255, 0.3);
     }
+  }
+  select {
+  width: 320px;
+  height: 46px;
+  padding-left: 16px;
+  border: 1px solid ${props => props.theme.gray};
+  border-radius: 8px;
+  margin-top: 8px;
+  font-family: 'Pretendard', sans-serif;
+  /* 브라우저 기본 디자인 숨기기 */
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  /* 새로운 화살표 디자인 추가 */
+  background: url(/arrow.png) calc(100% - 16px) center no-repeat;
+  background-size: 14px;
+  background-color: ${props => props.theme.white};
+  box-sizing: border-box;
+  transition: all 0.3s;
+  &:focus {
+    outline: none;
+    border: 1px solid ${props => props.theme.secondary};
+    box-shadow: 0 0 6px 3px rgba(156, 184, 255, 0.3);
+    }
+  &:required:invalid {
+    color: gray;
+  }
+  select,
+  option {
+    color: black;
+  }
   }
 `;
 
